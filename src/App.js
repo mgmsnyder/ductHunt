d058d25bc3b2033d5f6887136d8e8a447b132c29
Mathew Snyder <mgmsnyder@gmail.com>
2021-10-21 11:24:20 -0400

    Updated dependencies and made formatting changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from "react";
-import "./App.css";
-import Form from "./Form";
+import React from 'react';
+import './App.css';
+import Form from './Form';
 
 function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>Duct Hunt!</h1>
-				<h2>Report spam callers with speed</h2>
-			</header>
-			<Form />
-		</div>
-	);
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Duct Hunt!</h1>
+        <h2>Report spam callers with speed</h2>
+      </header>
+      <Form />
+    </div>
+  );
 }
 
 export default App;
