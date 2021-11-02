c9decc4b92cedf823b328de30494a3620d99f281
Mathew Snyder <mgmsnyder@gmail.com>
2021-11-02 07:38:00 -0400

    removed leftover code dealing with error messages

--- src/submitSite.js
+++ src/submitSite.js
@@ -7,18 +7,19 @@ import { Builder, By, Key, until } from 'selenium-webdriver';
   // );
   try {
     // Navigate to Url
-    await driver.get('https://www.google.com');
+    await driver.get('https://lnnte-dncl.gc.ca/en/Consumer/Complaint/#!/');
 
     // Enter text "cheese" and perform keyboard action "Enter"
-    await driver.findElement(By.name('q')).sendKeys('cheese', Key.ENTER);
+    await driver.findElement(By.id('radio-answer-phone')).click();
 
-    let firstResult = await driver.wait(
-      until.elementLocated(By.css('h3')),
-      10000
-    );
+    // let firstResult = await driver.wait(
+    //   until.elementLocated(By.css('h3')),
+    //   10000
+    // );
 
-    console.log(await firstResult.getAttribute('textContent'));
+    // console.log(await firstResult.getAttribute('textContent'));
   } finally {
-    await driver.quit();
+    // await driver.wait(false,10000);
+    // await driver.quit();
   }
 })();
