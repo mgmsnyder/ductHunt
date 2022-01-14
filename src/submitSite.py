10a924074eeeca9cf865a328d44f5c00cfec57ee
Mathew Snyder <mgmsnyder@gmail.com>
2022-01-14 08:30:02 -0500

    Added driver action with wait to confirm syntax

--- src/submitSite.py
+++ src/submitSite.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.support.ui import WebDriverWait
 
 options = ChromeOptions()
 #config driver options before initiating driver
+
 driver = webdriver.Chrome(options=options)
+WebDriverWait(driver).until(driver.get('https://lnnte-dncl.gc.ca/en/Consumer/Complaint/#!/'))
+
+# WebDriverWait(driver).until(lambda d: d.find_element_by_tag_name("p"))
+
 driver.quit()
